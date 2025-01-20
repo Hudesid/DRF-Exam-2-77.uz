56abb3684847140d45de4a31e7c8588cf2be4ed6
Akbar <hudes342@gmail.com>
2025-01-20 08:14:00 +0500

    The project is more than half completed

--- announcement/views.py
+++ announcement/views.py
@@ -1,9 +1,14 @@
+from django.db.models import Q
 from django.views.generic import ListView
+from rest_framework import status
 from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated, BasePermission, IsAdminUser
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from . import serializers, models, paginations
 
 
@@ -113,3 +118,47 @@ class ProductDestroyAPIView(DestroyAPIView):
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductForGetSerializer
     permission_classes = [AuthorValidateProductAPIView]
+
+
+class SearchAPIView(APIView):
+    queryset = models.Product.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        search_query = request.GET.get('q', None)
+
+        if search_query is None:
+            posts = self.queryset
+        else:
+            search_filter = Q(
+                name__icontains=search_query
+            ) | Q(
+                price__icontains=search_query
+            ) | Q(
+                currency__icontains=search_query
+            ) | Q(
+                published_at__icontains=search_query
+            ) | Q(
+                category__name__icontains=search_query
+            ) | Q(
+                category__parent__name__icontains=search_query
+            ) | Q(
+                category__parent__parent__name__icontains=search_query
+            )
+
+            posts = self.queryset.filter(search_filter)
+
+        serializer = serializers.ProductSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class LogoutAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({'detail':'Successfully logged out.'})
+        except KeyError:
+            return Response({'detail':'Refresh token required.'})
