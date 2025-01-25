12918a1819fc2029b95f386412e23780b588a290
Akbar <hudesid342@gmail.com>
2025-01-25 14:39:31 +0500

    Project almost completed

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,10 @@ SIMPLE_JWT = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
 
