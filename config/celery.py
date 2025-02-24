5e752caa6cc03b9646c97771ffc75232d3a7ac20
Akbar <hudesid342@gmail.com>
2025-02-24 14:08:26 +0500

    Project almost completed

--- config/celery.py
+++ config/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('config', broker='redis://127.0.0.1:6379/0')
+app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
